b6726e50be0bfb41950eaf882763218fdb2877b1
ouahchi ons <ons.ouahchi@insat.u-carthage.tn>
2022-02-27 19:04:19 +0100

    atelier_js

--- ExRandomColor/script.js
+++ ExRandomColor/script.js
@@ -1,12 +1,6 @@
-function getRandomColor() {
-    let letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
 
 document.querySelector("ol").addEventListener("click",
-    (e)=> {e.target.style.color=getRandomColor();}
+    (e)=> {
+    e.target.style.color=e.target.style.color='#'+Math.floor(Math.random()*16777215).toString(16);
+}
     )
